f61518a90b41d0cb5819b9d27f1232ab918485c7
Nícolas dos Santos Carvalho <nicolas.mnw@gmail.com>
2024-03-26 17:16:26 -0300

    cursor implementado

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,16 @@
-use std::{io::{stdout, BufRead, Stdout}, process::exit};
+use std::
+    io::{stdout, Stdout}
+;
 
-use crossterm::{cursor, execute, terminal::{Clear, ClearType}, ExecutableCommand};
-use log::error;
+use crossterm::{
+    cursor::{self, MoveTo},
+    event::{read, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    ExecutableCommand,
+};
 
-use crate::editor::EditorV8;
+use crate::editor::{EditorContentTrait, EditorEvent, VectorEditor};
 
 pub struct ConsoleClient {
     stdout: Stdout,
@@ -11,44 +18,91 @@ pub struct ConsoleClient {
 }
 
 pub trait Client<T> {
+    fn load(&mut self);
+    fn update(&mut self, context: &mut T) -> Option<u8>;
     fn draw(&mut self, context: &T);
 }
 
 impl ConsoleClient {
     pub fn new(line_numbered: bool) -> Self {
-        Self { 
+        Self {
             stdout: stdout(),
-            line_numbered 
+            line_numbered,
         }
+    }
+
+    fn pre_draw(&mut self) -> Result<&mut Stdout, std::io::Error> {
+        self.stdout
+            .execute(Clear(ClearType::Purge))?
+            .execute(MoveTo(0, 0))
     }
 }
 
-impl Client<EditorV8> for ConsoleClient {
-    fn draw(&mut self, context: &EditorV8) {
+impl Client<VectorEditor> for ConsoleClient {
+    fn load(&mut self) {
+        enable_raw_mode().unwrap();
+
+        execute!(self.stdout, Clear(ClearType::All)).unwrap();
+    }
+
+    fn update(&mut self, context: &mut VectorEditor) -> Option<u8> {
+        match read().unwrap() {
+            Event::Key(key) => {
+                if key.kind == KeyEventKind::Release {
+                    return None;
+                }
+
+                match key.code {
+                    KeyCode::Char(c) => {
+                        context.on_write(c as u32);
+                    }
+                    KeyCode::Backspace => {
+                        context.on_write(0x08);
+                    }
+                    KeyCode::Enter => {
+                        context.on_write(0x0D);
+                    }
+                    KeyCode::Esc => {
+                        return Some(1);
+                    }
+                    _ => {}
+                }
+            }
+            _ => (),
+        }
+
+        None
+    }
+
+    fn draw(&mut self, context: &VectorEditor) {
         if context.file_path.is_none() {
             println!("no file provided!");
             return;
         }
-        
-        execute!(
-            self.stdout, 
-            Clear(ClearType::Purge),
-            cursor::MoveTo(0, 0)
-        ).unwrap_or_else(|_| {
-            error!("unable to setup console");
-            exit(1);
-        });
-        
-        let lines = context.content.lines().enumerate();
 
-        for (line_num, line) in lines {
-            let display_str = line.unwrap_or("<ERROR>".to_string());
-            
+        self.pre_draw().unwrap();
+
+        let mut line_num = 0;
+
+        while let Some(line) = context.content.get_line(line_num) {
+        // for (line_num, line) in lines {
             if self.line_numbered {
                 print!("{:>4}  ", line_num);
             }
-                
-            println!("{}", display_str);
+
+            println!("{}", line);
+            line_num += 1;
         }
+
+        let cursor_x = context.render_col + 6;
+        let cursor_y = context.row;
+
+        let _result = self.stdout.execute(cursor::MoveTo(cursor_x as u16, cursor_y as u16));
     }
+}
+
+impl Drop for ConsoleClient {
+    fn drop(&mut self) {
+        disable_raw_mode().unwrap()
+    }    
 }
