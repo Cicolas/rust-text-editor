d3f9f32c944040e4c0846b3dc19806fe811943eb
Nícolas Carvalho <nicolas.mnw@gmail.com>
2024-04-07 19:49:11 -0300

    added scroll x

--- src/client/console.rs
+++ src/client/console.rs
@@ -10,9 +10,14 @@ use crossterm::{
     terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType},
     ExecutableCommand,
 };
+use pad::PadStr;
 
-use crate::editor::{
-    vector::CharVectorEditor, Action, EditorContentTrait, EditorEvent, Mode, Movement, Redraw,
+use crate::{
+    editor::{
+        vector::CharVectorEditor, Action, Container, EditorContentTrait, EditorEvent, Mode,
+        Movement, Redraw,
+    },
+    utils::TruncAt,
 };
 
 use super::ClientEvent;
@@ -46,21 +51,23 @@ impl ConsoleClient {
         // self.stdout.execute(cursor::Hide)?.execute(MoveTo(0, 0))
     }
 
-    fn draw_line(&self, line_num: u32, content: String) {
+    fn draw_line(&self, line_num: u32, content: String, len: u32) {
         if self.line_numbered {
             print!("{:>4}  ", line_num + 1);
         }
 
+        let striped_content = content.with_exact_width(len as usize);
+
         if cfg!(target_os = "windows") {
-            println!("{}", content);
+            println!("{}", striped_content);
         } else {
-            println!("{}\r", content);
+            println!("{}\r", striped_content);
         }
     }
 
-    fn draw_cursor(&mut self, col: u32, row: u32, mode: Mode, view_start: u32) {
-        let render_col = col;
-        let render_row = row - view_start;
+    fn draw_cursor(&mut self, col: u32, row: u32, mode: Mode, view: &Container) {
+        let render_col = col - view.left;
+        let render_row = row - view.top;
 
         let carret = match mode {
             Mode::Normal => SetCursorStyle::SteadyBlock,
@@ -134,8 +141,11 @@ impl ClientEvent<CharVectorEditor> for ConsoleClient {
     fn load(&mut self, context: &mut CharVectorEditor) {
         enable_raw_mode().unwrap();
 
-        let (_, h) = terminal::size().unwrap();
-        context.on_action(vec![Action::Resize(h - 1)]);
+        let (w, h) = terminal::size().unwrap();
+        context.on_action(vec![Action::Resize(
+            if self.line_numbered { w - 6 } else { w },
+            h - 1,
+        )]);
 
         execute!(self.stdout, Clear(ClearType::All)).unwrap();
     }
@@ -157,7 +167,10 @@ impl ClientEvent<CharVectorEditor> for ConsoleClient {
 
                 context.on_action(actions);
             }
-            Ok(Event::Resize(_, h)) => context.on_action(vec![Action::Resize(h - 1)]),
+            Ok(Event::Resize(w, h)) => context.on_action(vec![Action::Resize(
+                if self.line_numbered { w - 6 } else { w },
+                h - 1,
+            )]),
             _ => (),
         }
 
@@ -174,23 +187,33 @@ impl ClientEvent<CharVectorEditor> for ConsoleClient {
             self.pre_draw(redraw).unwrap();
         }
 
-        let mut line_num = context.view_start;
+        let mut line_num = context.view.top;
 
         match context.should_redraw {
             Some(Redraw::All) => {
                 while let Some(line) = context.content.get_line(line_num) {
-                    if line_num > context.view_end {
+                    if line_num > context.view.bottom {
                         break;
                     }
-                    self.draw_line(line_num, line);
+                    self.draw_line(
+                        line_num,
+                        line.trucate_at(context.view.left as usize)
+                            .unwrap_or(String::default()),
+                        context.view.get_width(),
+                    );
                     line_num += 1;
                 }
             }
             Some(Redraw::Line(line_num)) => {
                 if let Some(line) = context.content.get_line(line_num) {
-                    let clear_row = cmp::max(0, line_num as i32 - context.view_start as i32);
+                    let clear_row = cmp::max(0, line_num as i32 - context.view.top as i32);
                     self.stdout.execute(MoveTo(0, clear_row as u16)).unwrap();
-                    self.draw_line(line_num, line);
+                    self.draw_line(
+                        line_num,
+                        line.trucate_at(context.view.left as usize)
+                            .unwrap_or(String::default()),
+                        context.view.get_width(),
+                    );
                 }
             }
             Some(Redraw::Range(_, _)) => todo!(),
@@ -201,7 +224,7 @@ impl ClientEvent<CharVectorEditor> for ConsoleClient {
             context.render_col,
             context.render_row,
             context.mode,
-            context.view_start,
+            &context.view,
         );
     }
 }
